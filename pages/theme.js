a813a326d7b00d4b67ea7f5762415b490d58fd12
Vince Lavador <vincedlavador@gmail.com>
2021-06-14 19:56:49 +0800

    sample

--- pages/theme.js
+++ pages/theme.js
@@ -1,6 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
-
+import React from 'react';
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
